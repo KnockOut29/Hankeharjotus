20491305609883cfa990cafb068777567c4611f4
KnockOut29 <hanna.tuominen97@gmail.com>
2024-01-25 22:46:37 +0200

    JS

--- JS app.js
+++ JS app.js
@@ -1,4 +1,36 @@
-function togglePopup(){
-  document.getElementById("popup-1").classList.toggle("active")
+/*toggle icon navbar*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
+
+/*scroll section active link*/
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height){
+          navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+          });
+      };
+    });
+
+    /*stiky navbar*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
 
